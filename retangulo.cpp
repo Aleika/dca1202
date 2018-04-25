de1197eb3bf372d7a5f6acec6dc2577c7def7320
Aleika <aleikaalves@hotmail.com>
2018-04-24 21:41:27 -0300

    Update retangulo.cpp

--- retangulo.cpp
+++ retangulo.cpp
@@ -7,7 +7,6 @@ Retangulo::Retangulo(){
 
 }
 Retangulo::Retangulo(float x, float y, float largura, float altura){
-//    poligono[npontos].setXY(x,y); npontos++;
     addponto(x,y);
     addponto(x,y-altura);
     addponto(x+largura,y-altura);
